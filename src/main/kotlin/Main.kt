a0b2f12859158add67f4bae2f6baeedc2bbb3337
zhangxuxuchang <44724385@qq.com>
2023-09-05 11:24:39 +0800

    accept input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
+import java.util.Scanner
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val scanner = Scanner(System.`in`)
+    while (true) {
+        println("请输入您的命令：")
+        when (scanner.nextLine()) {
+            "hello" -> println("您好！")
+            "bye" -> {
+                println("再见！")
+                break
+            }
+            else -> println("无法识别的命令，请重新输入。")
+        }
+    }
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Program finished arguments: ${args.joinToString()}")
 }
