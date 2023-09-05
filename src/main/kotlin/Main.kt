93de3665ebe07372df26bf86602c08db3ae278e6
zhangxuxuchang <44724385@qq.com>
2023-09-05 14:17:54 +0800

    add send msg command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,31 @@
 package org.okt.ilivechat
+
+import org.okt.ilivechat.chat.ChatService
+
 fun main(args: Array<String>) {
+    val chatService = ChatService()
+    println("输入 :help 显示使用帮助")
     while (true) {
-        println("请输入您的命令：")
-        when (readln()) {
-            "hello" -> println("您好！")
-            "bye" -> {
+        when (val msg = readln()) {
+            ":help" -> printHelp()
+            ":bye" -> {
                 println("再见！")
                 break
             }
-            else -> println("无法识别的命令，请重新输入。")
+            else -> chatService.handleMsg(msg)
         }
     }
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program finished arguments: ${args.joinToString()}")
+    println("Program Finished ${args.joinToString()}")
+}
+
+private fun printHelp() {
+    println("--------命令说明---------")
+    println(":help 帮助")
+    println(":bye 退出")
+    println(":login-用户ID整数 登录")
+    println(":room-房间ID整数 加入房间")
+    println("其余自由输入是聊天模式")
+    println("--------End---------")
 }
--- src/main/kotlin/chat/ChatService.kt
+++ src/main/kotlin/chat/ChatService.kt
@@ -1,4 +1,40 @@
 package org.okt.ilivechat.chat
-
 class ChatService {
+    private var userId = 0
+    private var roomId = 0
+    fun handleMsg(msg: String) {
+
+        if(msg.startsWith(":login-")){
+            val userId = msg.substringAfter(":login-").toInt()
+            if (userId > 0) {
+                this.userId = userId
+                println("用户[$userId]登录成功")
+            } else {
+                println("用户ID必须大于0")
+            }
+        }
+
+        if(msg.startsWith(":room-")) {
+            val roomId = msg.substringAfter(":room-").toInt()
+            if (roomId > 0) {
+                this.roomId = roomId
+                println("用户[$userId]加入房间[$roomId]成功")
+            } else {
+                println("房间ID必须大于0")
+            }
+        }
+        sendMsg(msg)
+    }
+
+    private fun sendMsg(msg: String) {
+        if (userId == 0) {
+            println("请先登录> 输入:login-用户ID整数")
+            return
+        }
+        if (roomId == 0) {
+            println("请先加入房间> 输入:room-房间ID整数")
+            return
+        }
+        println("用户[$userId]: $msg")
+    }
 }
